10f2c626a897f8449f49f953524c68ecb08427ef
Saeidp12 <43015344+Saeidp12@users.noreply.github.com>
2021-05-12 16:28:24 +0430

    Update plt.py
    Added Subplot, barplot, and Histogram

--- plt.py
+++ plt.py
@@ -23,4 +23,34 @@ plt.legend()
 plt.savefig('mygraph.png', dpi=100)  # LQ
 plt.savefig('mygraph.eps', format='eps', dpi=300)  # HQ
 plt.show()
-# %%
+
+# Fifa 21 Player Rankings
+fifa = pd.read_csv('fifa21.csv')
+
+print(fifa.shape)  # gives us both dimensions
+print(len(fifa))  # number of rows only
+
+# Count Values of a specified column
+print(fifa['preferred_foot'].value_counts())
+
+# Barplot
+plt.bar(['right', 'left'], fifa['preferred_foot'].value_counts())
+
+# Descriptive Statistics
+print(fifa['overall'].describe())
+
+# Bins required for histogram
+bins = [40, 50, 60, 70, 80, 90, 100]
+
+# Reset the Canvas Width and Height Ratio
+plt.figure(figsize=(8, 5))
+
+# Histogram of player overalls with specified color
+plt.hist(fifa['overall'], bins=bins, color='skyblue', edgecolor='blue')
+
+# Plot labels and title
+plt.xlabel('Player Overalls')
+plt.ylabel('Frequency')
+plt.title('Histogram of player overalls')
+
+plt.show()
